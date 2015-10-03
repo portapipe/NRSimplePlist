9497a4efbf6460ea2384b5f2680009621e25b3ce
Tim Broder <timothy.broder@gmail.com>
2015-10-03 12:12:16 -0400

    Updates for Swift 2

--- NRSimplePlist/NRSwiftPlist.swift
+++ NRSimplePlist/NRSwiftPlist.swift
@@ -7,76 +7,92 @@
 
 import UIKit
 
-func plistGet(key:String, forPlistNamed:String) -> AnyObject{
-    
-    var error:NSError?
-    var formats: UnsafeMutablePointer<NSPropertyListFormat> = UnsafeMutablePointer()
+public func plistGet(key:String, forPlistNamed:String) throws -> AnyObject{
+    let formats: UnsafeMutablePointer<NSPropertyListFormat> = UnsafeMutablePointer()
     
     var array:NSArray = NSArray()
     var tipo:AnyObject = ""
+    
+    enum NRSimplePlistError: ErrorType {
+        case Fatal(String)
+    }
     
     
     if let path:String = NSBundle.mainBundle().pathForResource(forPlistNamed, ofType: "plist"){
         
         if let plistData = NSData(contentsOfFile: path) {
             
-            var plist = NSPropertyListSerialization.propertyListWithData(plistData, options: 0, format: formats, error: &error) as NSDictionary
+            //var plist = NSPropertyListSerialization.propertyListWithData(plistData, options: 0, format: formats, error: &error) as NSDictionary
             
-            if (path=="") {
-                NSLog("NRSimplePlist - Errore LETTURA file plist '\(path)', errore = '\(error)'")
-                NSLog("NRSimplePlist - Error READING plist file '\(path)', error = '\(error)'")
+            do {
+                let plist = try NSPropertyListSerialization.propertyListWithData(plistData, options: NSPropertyListReadOptions.Immutable, format: formats) as! NSDictionary
+                
+                if (path=="") {
+                    NSLog("NRSimplePlist - Errore LETTURA file plist '\(path)'")
+                    NSLog("NRSimplePlist - Error READING plist file '\(path)'")
+                    
+                    throw NRSimplePlistError.Fatal("NRSimplePlist - Error READING plist file '\(path)'")
+                }
+                
+                tipo = plist.valueForKey(key)!
+                    
+                return tipo
+                
+            } catch let error1 as NSError {
+                NSLog("NRSimplePlist - Errore LETTURA file plist '\(path)'")
+                NSLog("NRSimplePlist - Error READING plist file '\(path)'")
+
+                throw error1
             }
-            
-            tipo = plist.valueForKey(key)!
-            
-            return tipo
             
         }
         
     } else {
-        println("File '\(forPlistNamed)' not found")
+        print("File '\(forPlistNamed)' not found")
     }
     
     return tipo
 }
 
-func plistSet(newValue:AnyObject, forKey:String, inPlistNamed:String){
-    
-    
-    var error:NSError?
-    var formats: UnsafeMutablePointer<NSPropertyListFormat> = UnsafeMutablePointer()
+public func plistSet(newValue:AnyObject, forKey:String, inPlistNamed:String) throws {
+    let formats: UnsafeMutablePointer<NSPropertyListFormat> = UnsafeMutablePointer()
     
     var array:NSMutableDictionary = NSMutableDictionary()
     
-    var fileManager : NSFileManager = NSFileManager.defaultManager()
+    let fileManager : NSFileManager = NSFileManager.defaultManager()
     
     
     if let path : NSString = NSBundle.mainBundle().pathForResource(inPlistNamed, ofType: "plist") {
         
-        if fileManager.fileExistsAtPath(path){
+        if fileManager.fileExistsAtPath(path as String){
             
             
-            if let plistData = NSData(contentsOfFile: path) {
+            if let plistData = NSData(contentsOfFile: path as String) {
                 
-                var plist = NSPropertyListSerialization.propertyListWithData(plistData, options: 0, format: formats, error: &error) as NSMutableDictionary
                 
-                if (path=="") {
-                    NSLog("NRSimplePlist - Errore LETTURA file plist '\(path)', errore = '\(error)'")
-                    NSLog("NRSimplePlist - Error READING plist file '\(path)', error = '\(error)'")
+                do {
+                    let plist = try NSPropertyListSerialization.propertyListWithData(plistData, options: NSPropertyListReadOptions.Immutable, format: formats) as! NSDictionary
+                    
+                    array = plist.mutableCopy() as! NSMutableDictionary
+                    
+                    array.setObject(newValue, forKey: forKey)
+                    array.writeToFile(path as String, atomically: true)
+                    
+                } catch let error1 as NSError {
+                    NSLog("NRSimplePlist - Errore LETTURA file plist '\(path)'")
+                    NSLog("NRSimplePlist - Error READING plist file '\(path)'")
+                    
+                    throw error1
                 }
-                
-                array = plist.mutableCopy() as NSMutableDictionary
-                
-                array.setObject(newValue, forKey: forKey)
-                array.writeToFile(path, atomically: true)
+  
             }
             
         } else {
-            println("File '\(inPlistNamed)' not found setting value \(newValue) for key \(forKey)")
+            print("File '\(inPlistNamed)' not found setting value \(newValue) for key \(forKey)")
         }
         
     }else{
-        println("NRswiftPlist - The file in the path doesn't exists")
+        print("NRswiftPlist - The file in the path doesn't exists")
     }
     
     
